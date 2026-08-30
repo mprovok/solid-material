f93ac6f3d9fc0e9c1bf282e88b2b0dbdee7e26c9
Mark Provo Kluit <mprovok@proton.me>
2026-08-30 15:27:54 +0200

    refactor(material): deduplicate some code using Dynamic

--- packages/material/src/components/select/MaterialSelect.tsx
+++ packages/material/src/components/select/MaterialSelect.tsx
@@ -1,11 +1,11 @@
 import '@material/web/select/filled-select.js';
 import '@material/web/select/outlined-select.js';
-import type { FlowComponent, VoidComponent } from 'solid-js';
+import type { VoidComponent } from 'solid-js';
 
 import { FilledSelect } from '@material/web/select/internal/filled-select';
 import { OutlinedSelect } from '@material/web/select/internal/outlined-select';
 import { createMediaQuery } from '@solid-primitives/media';
-import { Index, splitProps } from 'solid-js';
+import { Index } from 'solid-js';
 import { Dynamic } from 'solid-js/web';
 
 import { MaterialSelectOption } from './MaterialSelectOption';
@@ -41,79 +41,38 @@ export interface MaterialSelectProps {
   onChange?: (event: Event, value: string, name: string | undefined) => void;
 }
 
-interface MaterialSelectWithChildrenProps extends Omit<MaterialSelectProps, 'variant' | 'options'> {
-  quick?: boolean;
-}
-
-const MaterialFilledSelect: FlowComponent<MaterialSelectWithChildrenProps> = props => {
-  const onChange = (name: string | undefined, event: Event) => {
-    if (event.target instanceof FilledSelect) {
-      props.onChange?.(event, event.target.value, name);
-    }
-  };
-
-  return (
-    <md-filled-select
-      attr:id={props.id}
-      attr:name={props.name}
-      attr:label={props.label}
-      attr:value={props.value}
-      bool:error={props.showError}
-      attr:error-text={props.errorText}
-      attr:supporting-text={props.supportingText}
-      bool:required={props.required}
-      bool:disabled={props.disabled}
-      bool:quick={props.quick}
-      bool:no-asterisk={props.noAsterisk}
-      attr:aria-label={props.ariaLabel}
-      onChange={[onChange, props.name]}
-    >
-      {props.children}
-    </md-filled-select>
-  );
-};
-
-const MaterialOutlinedSelect: FlowComponent<MaterialSelectWithChildrenProps> = props => {
-  const onChange = (name: string | undefined, event: Event) => {
-    if (event.target instanceof OutlinedSelect) {
-      props.onChange?.(event, event.target.value, name);
-    }
-  };
-
-  return (
-    <md-outlined-select
-      attr:id={props.id}
-      attr:name={props.name}
-      attr:label={props.label}
-      attr:value={props.value}
-      bool:error={props.showError}
-      attr:error-text={props.errorText}
-      attr:supporting-text={props.supportingText}
-      bool:required={props.required}
-      bool:disabled={props.disabled}
-      bool:quick={props.quick}
-      bool:no-asterisk={props.noAsterisk}
-      attr:aria-label={props.ariaLabel}
-      onChange={[onChange, props.name]}
-    >
-      {props.children}
-    </md-outlined-select>
-  );
-};
-
-const selects: Record<MaterialSelectVariant, FlowComponent<MaterialSelectWithChildrenProps>> = {
-  filled: MaterialFilledSelect,
-  outlined: MaterialOutlinedSelect
+const selects: Record<MaterialSelectVariant, string> = {
+  filled: 'md-filled-select',
+  outlined: 'md-outlined-select'
 };
 
 export const MaterialSelect: VoidComponent<MaterialSelectProps> = props => {
-  const [localProps, otherProps] = splitProps(props, ['variant', 'options']);
-
   const prefersReducedMotion = createMediaQuery('(prefers-reduced-motion: reduce)');
 
+  const onChange = (name: string | undefined, event: Event) => {
+    if (event.target instanceof FilledSelect || event.target instanceof OutlinedSelect) {
+      props.onChange?.(event, event.target.value, name);
+    }
+  };
+
   return (
-    <Dynamic component={selects[localProps.variant]} {...otherProps} quick={prefersReducedMotion()}>
-      <Index each={localProps.options}>
+    <Dynamic
+      component={selects[props.variant]}
+      attr:id={props.id}
+      attr:name={props.name}
+      attr:label={props.label}
+      attr:value={props.value}
+      bool:error={props.showError}
+      attr:error-text={props.errorText}
+      attr:supporting-text={props.supportingText}
+      bool:required={props.required}
+      bool:disabled={props.disabled}
+      bool:quick={prefersReducedMotion()}
+      bool:no-asterisk={props.noAsterisk}
+      attr:aria-label={props.ariaLabel}
+      onChange={[onChange, props.name]}
+    >
+      <Index each={props.options}>
         {option => (
           <MaterialSelectOption value={option().value} disabled={option().disabled} ariaLabel={option().ariaLabel}>
             {option().label}
--- packages/material/src/components/text-field/MaterialTextField.tsx
+++ packages/material/src/components/text-field/MaterialTextField.tsx
@@ -67,119 +67,62 @@ export interface MaterialTextFieldProps {
   onInput?: (name: string | undefined, event: InputEvent) => void;
 }
 
-const MaterialFilledTextField: ParentComponent<MaterialTextFieldProps> = props => {
-  const onChange = (name: string | undefined, event: Event) => {
-    if (event.target instanceof TextField) {
-      props.onChange?.(name, event.target.value, event);
-    }
-  };
-
-  const onInput = (name: string | undefined, event: InputEvent) => {
-    props.onInput?.(name, event);
-  };
-
-  return (
-    <md-filled-text-field
-      attr:type={props.type}
-      attr:label={props.label}
-      attr:placeholder={props.placeholder}
-      attr:value={props.value}
-      attr:aria-label={props.ariaLabel}
-      attr:rows={props.rows}
-      attr:cols={props.columns}
-      bool:no-asterisk={props.noAsterisk}
-      attr:text-direction={props.textDirection}
-      bool:required={props.required}
-      bool:disabled={props.disabled}
-      bool:readonly={props.readOnly}
-      bool:multiple={props.multipleEmail}
-      bool:no-spinner={props.noSpinner}
-      attr:min={props.minimum}
-      attr:max={props.maximum}
-      attr:step={props.step}
-      attr:minlength={props.minLength}
-      attr:maxlength={props.maxLength}
-      attr:pattern={props.pattern}
-      attr:id={props.id}
-      attr:name={props.name}
-      attr:autocomplete={props.autocomplete ?? 'off'}
-      bool:error={props.showError}
-      attr:error-text={props.errorText}
-      attr:prefix-text={props.prefixText}
-      attr:suffix-text={props.suffixText}
-      attr:supporting-text={props.supportingText}
-      bool:has-leading-icon={props.icon !== undefined && props.iconPosition !== 'end'}
-      bool:has-trailing-icon={props.icon !== undefined && props.iconPosition === 'end'}
-      onChange={[onChange, props.name]}
-      onInput={[onInput, props.name]}
-    >
-      {props.children}
-      <Show when={props.icon}>
-        <MaterialIcon slot={props.iconPosition === 'end' ? 'trailing-icon' : 'leading-icon'}>{props.icon}</MaterialIcon>
-      </Show>
-    </md-filled-text-field>
-  );
-};
-
-const MaterialOutlinedTextField: ParentComponent<MaterialTextFieldProps> = props => {
-  const onChange = (name: string | undefined, event: Event) => {
-    if (event.target instanceof TextField) {
-      props.onChange?.(name, event.target.value, event);
-    }
-  };
-
-  const onInput = (name: string | undefined, event: InputEvent) => {
-    props.onInput?.(name, event);
-  };
-
-  return (
-    <md-outlined-text-field
-      attr:type={props.type}
-      attr:label={props.label}
-      attr:placeholder={props.placeholder}
-      attr:value={props.value}
-      attr:aria-label={props.ariaLabel}
-      attr:rows={props.rows}
-      attr:cols={props.columns}
-      bool:no-asterisk={props.noAsterisk}
-      attr:text-direction={props.textDirection}
-      bool:required={props.required}
-      bool:disabled={props.disabled}
-      bool:readonly={props.readOnly}
-      bool:multiple={props.multipleEmail}
-      bool:no-spinner={props.noSpinner}
-      attr:min={props.minimum}
-      attr:max={props.maximum}
-      attr:step={props.step}
-      attr:minlength={props.minLength}
-      attr:maxlength={props.maxLength}
-      attr:pattern={props.pattern}
-      attr:id={props.id}
-      attr:name={props.name}
-      attr:autocomplete={props.autocomplete ?? 'off'}
-      bool:error={props.showError}
-      attr:error-text={props.errorText}
-      attr:prefix-text={props.prefixText}
-      attr:suffix-text={props.suffixText}
-      attr:supporting-text={props.supportingText}
-      bool:has-leading-icon={props.icon !== undefined && props.iconPosition !== 'end'}
-      bool:has-trailing-icon={props.icon !== undefined && props.iconPosition === 'end'}
-      onChange={[onChange, props.name]}
-      onInput={[onInput, props.name]}
-    >
-      {props.children}
-      <Show when={props.icon}>
-        <MaterialIcon slot={props.iconPosition === 'end' ? 'trailing-icon' : 'leading-icon'}>{props.icon}</MaterialIcon>
-      </Show>
-    </md-outlined-text-field>
-  );
-};
-
-const textFields: Record<MaterialTextFieldVariant, ParentComponent<MaterialTextFieldProps>> = {
-  filled: MaterialFilledTextField,
-  outlined: MaterialOutlinedTextField
+const textFields: Record<MaterialTextFieldVariant, string> = {
+  filled: 'md-filled-text-field',
+  outlined: 'md-outlined-text-field'
 };
 
 export const MaterialTextField: ParentComponent<MaterialTextFieldProps> = props => {
-  return <Dynamic component={textFields[props.variant]} {...props} />;
+  const onChange = (name: string | undefined, event: Event) => {
+    if (event.target instanceof TextField) {
+      props.onChange?.(name, event.target.value, event);
+    }
+  };
+
+  const onInput = (name: string | undefined, event: InputEvent) => {
+    props.onInput?.(name, event);
+  };
+
+  return (
+    <Dynamic
+      component={textFields[props.variant]}
+      attr:type={props.type}
+      attr:label={props.label}
+      attr:placeholder={props.placeholder}
+      attr:value={props.value}
+      attr:aria-label={props.ariaLabel}
+      attr:rows={props.rows}
+      attr:cols={props.columns}
+      bool:no-asterisk={props.noAsterisk}
+      attr:text-direction={props.textDirection}
+      bool:required={props.required}
+      bool:disabled={props.disabled}
+      bool:readonly={props.readOnly}
+      bool:multiple={props.multipleEmail}
+      bool:no-spinner={props.noSpinner}
+      attr:min={props.minimum}
+      attr:max={props.maximum}
+      attr:step={props.step}
+      attr:minlength={props.minLength}
+      attr:maxlength={props.maxLength}
+      attr:pattern={props.pattern}
+      attr:id={props.id}
+      attr:name={props.name}
+      attr:autocomplete={props.autocomplete ?? 'off'}
+      bool:error={props.showError}
+      attr:error-text={props.errorText}
+      attr:prefix-text={props.prefixText}
+      attr:suffix-text={props.suffixText}
+      attr:supporting-text={props.supportingText}
+      bool:has-leading-icon={props.icon !== undefined && props.iconPosition !== 'end'}
+      bool:has-trailing-icon={props.icon !== undefined && props.iconPosition === 'end'}
+      onChange={[onChange, props.name]}
+      onInput={[onInput, props.name]}
+    >
+      {props.children}
+      <Show when={props.icon}>
+        <MaterialIcon slot={props.iconPosition === 'end' ? 'trailing-icon' : 'leading-icon'}>{props.icon}</MaterialIcon>
+      </Show>
+    </Dynamic>
+  );
 };
