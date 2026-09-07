d90ddde50e976b177a4841ed3cdea5932d1418a9
Mark Provo Kluit <mprovok@proton.me>
2026-09-07 18:18:20 +0200

    chore(material): split vitest config into 2 projects
    Define separate projects for component testing using Vitest's Browser
    Mode and for running the Storybook tests.

--- packages/material/vitest.config.ts
+++ packages/material/vitest.config.ts
@@ -8,18 +8,8 @@ import { defineConfig } from 'vitest/config';
 
 // oxlint-disable-next-line import/no-default-export
 export default defineConfig({
-  plugins: [
-    solidPlugin(),
-    solidSvg(),
-    // The plugin will run tests for the stories defined in your Storybook config
-    // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-    storybookTest({
-      configDir: path.join(import.meta.dirname, '.storybook'),
-      storybookScript: 'pnpm storybook --no-open'
-    })
-  ],
+  plugins: [solidPlugin(), solidSvg()],
   test: {
-    name: 'storybook',
     environment: 'node',
     browser: {
       provider: playwright(),
@@ -35,6 +25,30 @@ export default defineConfig({
       provider: 'istanbul',
       reporter: [['cobertura', { file: 'coverage.xml' }], ['text']],
       reportOnFailure: true
-    }
+    },
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: 'storybook'
+        },
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          storybookTest({
+            configDir: path.join(import.meta.dirname, '.storybook'),
+            storybookScript: 'pnpm storybook --no-open'
+          })
+        ]
+      },
+      {
+        extends: true,
+        test: {
+          name: 'components',
+          include: ['src/**/*.test.tsx'],
+          setupFiles: 'vitest.setup.ts'
+        }
+      }
+    ]
   }
 });
