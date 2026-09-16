be3d19c0015b436f232cd148c0ce96587b4be562
Mark Provo Kluit <mprovok@proton.me>
2026-09-16 21:26:51 +0200

    fix(material): fix spacer near edges of fixed-flexible layout
    In a fixed-flexible layout, when dragging the spacer all the way to the
    edge of the layout, the width and position of the spacer started to
    transition to their final values further from the edge than expected.

---
+++ .changeset/legal-needles-grin.md
@@ -0,0 +1,5 @@
+---
+'@solidmaterial/material': patch
+---
+
+Fix computing width and position of spacer near edges of fixed-flexible layout
--- packages/material/src/layouts/body-layout/pane-layouts/fixed-flexible-layout/FixedFlexibleLayout.tsx
+++ packages/material/src/layouts/body-layout/pane-layouts/fixed-flexible-layout/FixedFlexibleLayout.tsx
@@ -15,7 +15,7 @@ export const FixedFlexibleLayout: FlowComponent<FixedAndFlexiblePaneLayoutProps>
 
   const snapWidths = () => [0, ...props.snapWidths, maximumWidth()];
 
-  const offsetLeft = () => clamp(0, props.width - SPACER_WIDTH, SPACER_WIDTH);
+  const offsetLeft = () => clamp(0, props.width, SPACER_WIDTH);
   const offsetRight = () => clamp(0, props.maximumWidth - props.width - SPACER_WIDTH, SPACER_WIDTH);
 
   const clampedWidth = () => clamp(0, props.width, maximumWidth());
